4bb873db101158faefea96c1ecadb8622739f6f4
Aaron Krajeski <ak@aaronk.is>
2016-10-04 07:52:56 -0400

    Change indentation. I had to. Sorry.

--- ChiselTipGeometry.js
+++ ChiselTipGeometry.js
@@ -1,68 +1,68 @@
 // Copyright 2016 Gracious Eloise, Inc. All rights reserved.
 
 ChiselTipGeometry = function(strokeData, degrees, lineWidth) {
-  var radians = degrees * 3.14 / 180;
-  var chiselTip = new THREE.Vector3(Math.sin(radians), Math.cos(radians), 0);
-  chiselTip.multiplyScalar(lineWidth/2);
+    var radians = degrees * 3.14 / 180;
+    var chiselTip = new THREE.Vector3(Math.sin(radians), Math.cos(radians), 0);
+    chiselTip.multiplyScalar(lineWidth/2);
 
-  var positions = [];
-  var times = [];
-  var lastDeltaZ;
+    var positions = [];
+    var times = [];
+    var lastDeltaZ;
 
-  var v = new THREE.Vector3();
-  var up = new THREE.Vector3();
-  var down = new THREE.Vector3();
+    var v = new THREE.Vector3();
+    var up = new THREE.Vector3();
+    var down = new THREE.Vector3();
 
-  for (var i = 0; i < strokeData.length-3; i += 4) {
-    var x = strokeData[i];
-    var y = strokeData[i+1];
-    var t = strokeData[i+2];
-    var deltaZ = strokeData[i+3];
+    for (var i = 0; i < strokeData.length-3; i += 4) {
+        var x = strokeData[i];
+        var y = strokeData[i+1];
+        var t = strokeData[i+2];
+        var deltaZ = strokeData[i+3];
 
-    // We are "in the gap" between strokes if the pen lifted before this and
-    // dropped after this, so we add degenerate triangles here:
-    var inTheGap = lastDeltaZ !== undefined && lastDeltaZ > 0 && deltaZ < 0;
+        // We are "in the gap" between strokes if the pen lifted before this and
+        // dropped after this, so we add degenerate triangles here:
+        var inTheGap = lastDeltaZ !== undefined && lastDeltaZ > 0 && deltaZ < 0;
 
-    if (inTheGap) {
-      positions.push(positions[positions.length-3], positions[positions.length-2], positions[positions.length-1]);
-      times.push(times[times.length-1]);
+        if (inTheGap) {
+            positions.push(positions[positions.length-3], positions[positions.length-2], positions[positions.length-1]);
+            times.push(times[times.length-1]);
+        }
+
+        v.set(x, y, 0);
+
+        up.subVectors(v, chiselTip);
+        positions.push(up.x, up.y, up.z);
+        times.push(t);
+
+        if (inTheGap) {
+            positions.push(positions[positions.length-3], positions[positions.length-2], positions[positions.length-1]);
+            times.push(times[times.length-1]);
+        }
+
+        down.addVectors(v, chiselTip);
+        positions.push(down.x, down.y, down.z);
+        times.push(t);
+
+        lastDeltaZ = deltaZ;
     }
 
-    v.set(x, y, 0);
+    // Normalize time
+    var maxTime = times.reduce(function(t, max) {
+        if (t > max) { return t; }
+        return max;
+    })
+    times = times.map(function(t) { return t / maxTime });
+    this.maxTime = 1.0;
 
-    up.subVectors(v, chiselTip);
-    positions.push(up.x, up.y, up.z);
-    times.push(t);
+    THREE.BufferGeometry.call(this);
+    this.type = 'ChiselTipGeometry';
+    this.addAttribute("position", new THREE.BufferAttribute(new Float32Array(positions), 3));
+    this.addAttribute("time", new THREE.BufferAttribute(new Float32Array(times), 1));
 
-    if (inTheGap) {
-      positions.push(positions[positions.length-3], positions[positions.length-2], positions[positions.length-1]);
-      times.push(times[times.length-1]);
-    }
-
-    down.addVectors(v, chiselTip);
-    positions.push(down.x, down.y, down.z);
-    times.push(t);
-
-    lastDeltaZ = deltaZ;
-  }
-
-  // Normalize time
-  var maxTime = times.reduce(function(t, max) {
-    if (t > max) { return t; }
-    return max;
-  })
-  times = times.map(function(t) { return t / maxTime });
-  this.maxTime = 1.0;
-
-  THREE.BufferGeometry.call(this);
-  this.type = 'ChiselTipGeometry';
-  this.addAttribute("position", new THREE.BufferAttribute(new Float32Array(positions), 3));
-  this.addAttribute("time", new THREE.BufferAttribute(new Float32Array(times), 1));
-
-  this.computeBoundingBox();
-  var minX = this.boundingBox.min.x,
-      minY = this.boundingBox.min.y;
-  this.translate(-minX, -minY, 0);
+    this.computeBoundingBox();
+    var minX = this.boundingBox.min.x,
+            minY = this.boundingBox.min.y;
+    this.translate(-minX, -minY, 0);
 
 }
 
--- ChiselTipMesh.js
+++ ChiselTipMesh.js
@@ -1,66 +1,66 @@
 // Copyright 2016 Gracious Eloise, Inc. All rights reserved.
 
 ChiselTipMesh = function(options) {
-  var strokeData = options.strokeData;
-  var degrees = options.degrees;
-  var lineWidth = options.lineWidth;
-  var color = options.color;
+    var strokeData = options.strokeData;
+    var degrees = options.degrees;
+    var lineWidth = options.lineWidth;
+    var color = options.color;
 
-  var duration = 0.5; // seconds for the entire animation
+    var duration = 0.5; // seconds for the entire animation
 
-  var geometry = new ChiselTipGeometry(strokeData, degrees, lineWidth);
+    var geometry = new ChiselTipGeometry(strokeData, degrees, lineWidth);
 
-  var material = new THREE.ShaderMaterial({
-    uniforms: {
-      color: { value: color },
-      speed: { value: 2.0 }, // animation speed factor (1.0 will show at the same speed as capture)
-      clock: { value: 0.0 },
-    },
-    vertexShader: `
-      attribute float time;
-      uniform float speed;
-      uniform float clock;
-      varying float age;
-      void main() {
-        age = clock * speed - time;
-        gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
-      }`,
-    fragmentShader: `
-      uniform vec3 color;
-      varying float age;
-      void main() {
-        gl_FragColor = vec4(color, 0.0);
-        if (age > 0.0) {
-          gl_FragColor.a = 1.0;
+    var material = new THREE.ShaderMaterial({
+        uniforms: {
+            color: { value: color },
+            speed: { value: 2.0 }, // animation speed factor (1.0 will show at the same speed as capture)
+            clock: { value: 0.0 },
+        },
+        vertexShader: `
+            attribute float time;
+            uniform float speed;
+            uniform float clock;
+            varying float age;
+            void main() {
+                age = clock * speed - time;
+                gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+            }`,
+        fragmentShader: `
+            uniform vec3 color;
+            varying float age;
+            void main() {
+                gl_FragColor = vec4(color, 0.0);
+                if (age > 0.0) {
+                    gl_FragColor.a = 1.0;
+                }
+            }`,
+        side: THREE.DoubleSide,
+        transparent: true,
+    });
+
+    THREE.Mesh.call(this, geometry, material);
+    this.drawMode = THREE.TriangleStripDrawMode;
+
+    material.uniforms.speed.value = geometry.maxTime / duration;
+
+    var startTimeSeconds;
+    var update = function(payload) {
+        var timeSeconds = Date.now()/1000;
+        material.uniforms.clock.value = timeSeconds - startTimeSeconds;
+        if (material.uniforms.clock.value < duration) {
+            requestAnimationFrame(update);
         }
-      }`,
-    side: THREE.DoubleSide,
-    transparent: true,
-  });
-
-  THREE.Mesh.call(this, geometry, material);
-  this.drawMode = THREE.TriangleStripDrawMode;
-
-  material.uniforms.speed.value = geometry.maxTime / duration;
-
-  var startTimeSeconds;
-  var update = function(payload) {
-    var timeSeconds = Date.now()/1000;
-    material.uniforms.clock.value = timeSeconds - startTimeSeconds;
-    if (material.uniforms.clock.value < duration) {
-      requestAnimationFrame(update);
     }
-  }
-  this.start = function() {
-    startTimeSeconds = Date.now()/1000;
-    requestAnimationFrame(update);
-  }
-  // Report how long this animation should run. This might need to be richer
-  // data, such as some indication that an animation does something
-  // significant for 1 second, then loops forever.
-  this.getLifetime = function() {
-    return duration;
-  }
+    this.start = function() {
+        startTimeSeconds = Date.now()/1000;
+        requestAnimationFrame(update);
+    }
+    // Report how long this animation should run. This might need to be richer
+    // data, such as some indication that an animation does something
+    // significant for 1 second, then loops forever.
+    this.getLifetime = function() {
+        return duration;
+    }
 }
 
 ChiselTipMesh.prototype = Object.create(THREE.Mesh.prototype);
